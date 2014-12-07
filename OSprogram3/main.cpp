c2c65c5ae596255162f2a3448f4da2d9d7acb66b
Bjornkjohnson <bjornjohnson@Bjorns-iMac.local>
2014-12-07 11:17:51 -0500

    Initial Commit

---
+++ OSprogram3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OSprogram3
+//
+//  Created by Bjorn Johnson on 12/7/14.
+//  Copyright (c) 2014 Bjorn Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
